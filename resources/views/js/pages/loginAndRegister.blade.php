9dc4f7d82fdabda15ea30ab0316ff26c0e4787af
orçun candan <orcuncandan89@gmail.com>
2024-08-30 15:38:30 +0300

    logout

--- resources/views/js/pages/loginAndRegister.blade.php
+++ resources/views/js/pages/loginAndRegister.blade.php
@@ -3,32 +3,33 @@
         <div class="row">
             <div class="col-6">
                 <h1 class="text-center">Login</h1>
-                <form action="" method="post">
+                <form onsubmit="return LoginAndRegister.login();">
+                    <div class="message"></div>
                     <div class="form-group mb-3">
-                        <label for="email">Email</label>
-                        <input type="email" class="form-control" id="email" name="email" placeholder="Enter email">
+                        <label for="loginAndRegisterView_email">Email</label>
+                        <input type="email" class="form-control" id="loginAndRegisterView_email" name="email" placeholder="Enter email">
                     </div>
                     <div class="form-group mb-3">
-                        <label for="password">Password</label>
-                        <input type="password" class="form-control" id="password" name="password" placeholder="Enter password">
+                        <label for="loginAndRegisterView_password">Password</label>
+                        <input type="password" class="form-control" id="loginAndRegisterView_password" name="password" placeholder="Enter password">
                     </div>
                     <button type="submit" class="btn btn-primary w-100">Login</button>
                 </form>
             </div>
             <div class="col-6">
                 <h1 class="text-center">Sign in</h1>
-                <form action="" method="post">
+                <form onsubmit="return LoginAndRegister.register();">
                     <div class="form-group mb-3">
-                        <label for="name">Name</label>
-                        <input type="text" class="form-control" id="name" name="name" placeholder="Enter name">
+                        <label for="loginAndRegisterView_name">Name</label>
+                        <input type="text" class="form-control" id="loginAndRegisterView_name" name="name" placeholder="Enter name">
                     </div>
                     <div class="form-group mb-3">
-                        <label for="email2">Email</label>
-                        <input type="email" class="form-control" id="email2" name="email" placeholder="Enter email">
+                        <label for="loginAndRegisterView_email2">Email</label>
+                        <input type="email" class="form-control" id="loginAndRegisterView_email2" name="email" placeholder="Enter email">
                     </div>
                     <div class="form-group mb-3">
-                        <label for="password2">Password</label>
-                        <input type="password" class="form-control" id="password2" name="password" placeholder="Enter password">
+                        <label for="loginAndRegisterView_password2">Password</label>
+                        <input type="password" class="form-control" id="loginAndRegisterView_password2" name="password" placeholder="Enter password">
                     </div>
                     <button type="submit" class="btn btn-primary w-100">Register</button>
                 </form>
@@ -44,4 +45,30 @@ Router.set('loginAndRegister', function() {
         document.getElementById('loginAndRegisterView').innerHTML
     );
 });
+
+Router.set('logout', function() {
+    Auth.logout();
+    Router.go('loginAndRegister');
+});
+
+class LoginAndRegister {
+    static login() {
+        let email=document.getElementById('loginAndRegisterView_email').value;
+        let password=document.getElementById('loginAndRegisterView_password').value;
+
+        var status=Auth.login(email, password, function (data) {
+            if (data.success) {
+                Router.go('home');
+                return true;
+            } else {
+                document.querySelector('.message').innerHTML=data.message;
+            }
+        });
+        return false;
+    }
+    static register(name, email, password) {
+        Auth.register(name, email, password);
+        return false;
+    }
+}
 </script>
