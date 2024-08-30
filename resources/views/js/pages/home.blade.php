702678ac8b837a122ab8ee9b0a9ee44affef44af
orçun candan <orcuncandan89@gmail.com>
2024-08-30 15:38:05 +0300

    Update home.blade.php

--- resources/views/js/pages/home.blade.php
+++ resources/views/js/pages/home.blade.php
@@ -1,10 +1,13 @@
 <div class="view-pages" id="homeView">
     <div class="contanier">
-       <h1>Welcome</h1>
+       <h1>Welcome<span id="homeView_userName"></span></h1>
     </div>
 </div>
 <script>
 Router.set('home', function() {
+    if (Auth.user) {
+        document.getElementById('homeView_userName').innerHTML=' '+Auth.user.name;
+    }
     view(
         'Home',
         document.getElementById('homeView').innerHTML
