c628e169b376242cfc021aa4f08dcf66bb24ce79
orçun candan <orcuncandan89@gmail.com>
2024-09-02 11:33:56 +0300

    api request class
    created to handle request more gracefull

---
+++ app/Http/Requests/ApiRequest.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Requests;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Exceptions\HttpResponseException;
+use Illuminate\Support\Facades\Auth;
+
+class ApiRequest extends FormRequest
+{
+    /**
+     * Failed auth override
+     *
+     * @throws \Illuminate\Http\Exceptions\HttpResponseException
+     *
+     * @return never
+     */
+    protected function failedAuthorization()
+    {
+        throw new HttpResponseException(
+            response()->json(
+                [
+                'message' => 'you are not authorized to do this.'
+                ], 403
+            )
+        );
+    }
+}
