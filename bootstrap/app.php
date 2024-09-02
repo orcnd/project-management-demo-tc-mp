5681ce83c90c011bdd6c10774b99c95d3f580128
orçun candan <orcuncandan89@gmail.com>
2024-09-02 12:02:03 +0300

    customizing api error response

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,6 +3,12 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
+use Symfony\Component\HttpKernel\Exception\HttpException;
+
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -11,17 +17,36 @@ return Application::configure(basePath: dirname(__DIR__))
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
     )
-    ->withMiddleware(function (Middleware $middleware) {
-        $middleware->api(prepend: [
-            \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
-        ]);
+    ->withMiddleware(
+        function (Middleware $middleware) {
+            $middleware->api(
+                prepend: [
+                \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
+                ]
+            );
 
-        $middleware->alias([
-            'verified' => \App\Http\Middleware\EnsureEmailIsVerified::class,
-        ]);
+            $middleware->alias(
+                [
+                'verified' => \App\Http\Middleware\EnsureEmailIsVerified::class,
+                ]
+            );
 
-        //
-    })
-    ->withExceptions(function (Exceptions $exceptions) {
-        //
-    })->create();
+            //
+        }
+    )
+    ->withExceptions(
+        function (Exceptions $exceptions) {
+            $exceptions->render(
+                function (NotFoundHttpException $e, Request $request) {
+                    if ($request->is('api/*')) {
+                        return response()->json(
+                            [
+                            'status' => false,
+                            'message' => 'Not found',
+                            ], 404
+                        );
+                    }
+                }
+            );
+        }
+    )->create();
