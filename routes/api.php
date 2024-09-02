578488f8562b70e88423e4f5b11f2bd95a2e5909
orçun candan <orcuncandan89@gmail.com>
2024-09-02 11:11:09 +0300

    add auth by sanctum token

--- routes/api.php
+++ routes/api.php
@@ -7,5 +7,12 @@ Route::controller(\App\Http\Controllers\AuthController::class)->group(
         Route::post('/login', 'login');
         Route::post('/register', 'register');
         Route::post('/logout', 'logout');
+        Route::get('/me', 'me')->middleware('auth:sanctum');
     }
 );
+
+
+Route::apiResource(
+    'projects',
+    \App\Http\Controllers\ProjectController::class
+)->middleware('auth:sanctum');
